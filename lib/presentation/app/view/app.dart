0e479f8eaac41802dd8a3de8be7474132a5211cd
Artem Fetissov <a.fetissov@andersenlab.com>
2022-05-30 07:46:40 +0000

    Update app.dart

--- lib/presentation/app/view/app.dart
+++ lib/presentation/app/view/app.dart
@@ -1,4 +1,25 @@
+import 'package:business_terminal/app/utils/l10n/generated/l10n.dart';
+import 'package:business_terminal/config/colors.dart';
+import 'package:business_terminal/dependency_injection/injectible_init.dart';
+import 'package:business_terminal/presentation/branch_profile/create_branch_profile_checkboxes_page/view/create_branch_profile_checkboxes_page.dart';
+import 'package:business_terminal/presentation/categories/cubit/subcategories_cubit.dart';
+import 'package:business_terminal/presentation/common/snackbar_manager.dart';
+import 'package:business_terminal/presentation/common/widgets/dashboard/cubit/dashboard_cubit.dart';
+import 'package:business_terminal/presentation/dashboard/account_verification/cubit/account_verification_cubit.dart';
+import 'package:business_terminal/presentation/dashboard/profile/profile_edit/cubit/profile_edit_cubit.dart';
+import 'package:business_terminal/presentation/navigation/app_state_cubit/app_state.dart';
+import 'package:business_terminal/presentation/navigation/app_state_cubit/app_state_cubit.dart';
+import 'package:business_terminal/presentation/navigation/app_state_cubit/authorized_state.dart';
+import 'package:business_terminal/presentation/navigation/app_state_cubit/unauthorize_state.dart';
+import 'package:business_terminal/presentation/navigation/splash_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
+final authNavigatorKey = GlobalKey<NavigatorState>();
+final unauthNavigatorKey = GlobalKey<NavigatorState>();
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -6,27 +27,102 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GlobalLoaderOverlay(
-      child: MaterialApp(
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-          colorScheme: ColorScheme.fromSwatch(
-            accentColor: const Color(0xFF13B9FF),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AppStateCubit>(
+            create: (_) => AppStateCubit(),
           ),
-          textTheme: GoogleFonts.interTextTheme(
-            Theme.of(context).textTheme.apply(
-                  bodyColor: const Color(0xff676f86),
-                  displayColor: const Color(0xff676f86),
-                ),
+          BlocProvider(
+            create: (_) => getIt.get<ProfileEditCubit>()..getInitialData(),
+          ),
+          BlocProvider<DashboardCubit>(
+            create: (_) => getIt.get<DashboardCubit>(),
+          ),
+          BlocProvider<AccountVerificationCubit>(
+            create: (_) => getIt.get<AccountVerificationCubit>(),
+          ),
+          BlocProvider<SubcategoriesCubit>(
+            create: (BuildContext context) {
+              return getIt.get<SubcategoriesCubit>();
+            },
+          )
+        ],
+        child: BlocBuilder<AppStateCubit, AppState>(
+          builder: (context, state) {
+            if (state is UnauthorizedState) {
+              return HamsterApp(
+                navKey: unauthNavigatorKey,
+                initialRoute: state.initialRoute,
+                onGenerateRoute: state.onGenerateRoute,
+              );
+            }
+
+            if (state is AuthorizedState) {
+              return HamsterApp(
+                navKey: authNavigatorKey,
+                initialRoute: state.initialRoute,
+                onGenerateRoute: state.onGenerateRoute,
+              );
+            }
+
+            return HamsterApp(
+              home: SlashPage(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class HamsterApp extends StatelessWidget {
+  const HamsterApp({
+    Key? key,
+    this.initialRoute,
+    this.onGenerateRoute,
+    this.home,
+    this.navKey,
+  }) : super(key: key);
+
+  final String? initialRoute;
+  final RouteFactory? onGenerateRoute;
+  final Widget? home;
+  final GlobalKey<NavigatorState>? navKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navKey,
+      debugShowCheckedModeBanner: false,
+      scaffoldMessengerKey: snackbarKey,
+      initialRoute: initialRoute,
+      onGenerateRoute: onGenerateRoute,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+        colorScheme: ColorScheme.fromSwatch(
+          accentColor: const Color(0xFF13B9FF),
+        ),
+        textTheme: GoogleFonts.interTextTheme(
+          Theme.of(context).textTheme.apply(
+                bodyColor: lynch,
+                displayColor: lynch,
+              ),
+        ),
+        checkboxTheme: CheckboxThemeData(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
           ),
         ),
-        supportedLocales: AppLocalizations.supportedLocales,
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        home: const RegistrationPage(),
       ),
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: Locale('de'),
+      home: home,
     );
   }
 }
